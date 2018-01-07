980b584497738355d0c622756314ec0cfa2c29c1
Ubuntu <fabricadmin@IR-chain-fabric2.v5esmqxuodzexbunz4gkidmwxc.lx.internal.cloudapp.net>
2018-01-07 07:23:28 +0000

    commit.

---
+++ first-network/restart.sh
@@ -0,0 +1,18 @@
+docker restart cli
+docker restart peer0.org1.example.com
+docker restart peer0.org2.example.com
+docker restart peer0.org3.example.com
+docker restart peer1.org1.example.com
+docker restart peer1.org2.example.com
+docker restart peer1.org3.example.com
+docker restart orderer.example.com
+docker restart ca_peerOrg1
+docker restart ca_peerOrg2
+docker restart ca_peerOrg3
+docker restart couchdb0
+docker restart couchdb1
+docker restart couchdb2
+docker restart couchdb3
+docker restart couchdb4
+docker restart couchdb5
+
